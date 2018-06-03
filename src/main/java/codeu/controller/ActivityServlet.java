2c018cc2a76f39ee7238e96929572fb5ab918edb
Kevin Villanueva <kevin_villanueva@umail.ucsb.edu>
2018-06-02 17:10:42 -0700

    Modified the activity feed servlet so that it could access conversations and modified the jsp file so that it loops through all the conversations and shows them

--- src/main/java/codeu/controller/ActivityServlet.java
+++ src/main/java/codeu/controller/ActivityServlet.java
@@ -1,27 +1,83 @@
 
-
 package codeu.controller;
 
+import codeu.model.data.Conversation;
+import codeu.model.data.Message;
 import java.io.IOException;
 
-import codeu.model.data.User;
+import codeu.model.store.basic.ConversationStore;
+import codeu.model.store.basic.MessageStore;
 import codeu.model.store.basic.UserStore;
+import java.util.List;
+import java.util.UUID;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/*Servlet class responsible for the activity feed page*/
 public class ActivityServlet extends HttpServlet {
+        
+    /*Store class that gives access to users*/
+	private UserStore userStore;
+    /*Store class that gives access to conversations*/
+	private ConversationStore conversationStore;
+	/** Store class that gives access to Messages. */
+	private MessageStore messageStore;
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws IOException, ServletException {
-    request.getRequestDispatcher("/WEB-INF/view/activity.jsp").forward(request, response);
-  }
 
-  @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws IOException, ServletException {
-  }
+	/**
+	* Set up state for handling conversation-related requests. This method is only called when
+	* running in a server, not when running in a test.
+	*/
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		setUserStore(UserStore.getInstance());
+		setConversationStore(ConversationStore.getInstance());
+		setMessageStore(MessageStore.getInstance());
+	}
+
+	/**
+	* Sets the UserStore used by this servlet. This function provides a common setup method for use
+	* by the test framework or the servlet's init() function.
+	*/
+	void setUserStore(UserStore userStore) {
+		this.userStore = userStore;
+	}
+
+	/**
+	* Sets the ConversationStore used by this servlet. This function provides a common setup method
+	* for use by the test framework or the servlet's init() function.
+	*/
+	void setConversationStore(ConversationStore conversationStore) {
+		this.conversationStore = conversationStore;
+	}
+
+	/**
+	* Sets the MessageStore used by this servlet. This function provides a common setup method for
+	* use by the test framework or the servlet's init() function.
+	*/
+	void setMessageStore(MessageStore messageStore) {
+	this.messageStore = messageStore;
+	}
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	throws IOException, ServletException {
+
+		/*Get all convos*/
+		List<Conversation> conversations = conversationStore.getAllConversations();
+
+		/*make the conversations variable accesible to the jsp file*/           
+        request.setAttribute("conversations", conversations);
+        
+		request.getRequestDispatcher("/WEB-INF/view/activity.jsp").forward(request, response);
+	}
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+	throws IOException, ServletException {
+	}
 }
--- src/main/webapp/WEB-INF/view/activity.jsp
+++ src/main/webapp/WEB-INF/view/activity.jsp
@@ -1,4 +1,14 @@
 
+<%@ page import="java.util.List" %>
+<%@ page import="codeu.model.data.Conversation" %>
+<%@ page import="codeu.model.data.Message" %>
+<%@ page import="codeu.model.store.basic.UserStore" %>
+<%
+List<Conversation> conversations = (List<Conversation>) request.getAttribute("conversations");
+// List<Message> messages = (List<Message>) request.getAttribute("messages");
+%>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,6 +16,25 @@
   <link rel="stylesheet" href="/css/main.css">
 </head>
 <body>
+
+<style>
+  #feed {
+    background-color: white;
+    height: 500px;
+    overflow-y: scroll
+  }
+</style>
+
+
+<script>
+    // scroll the feed div to the bottom
+      function scrollFeed() {
+        var feedDiv = document.getElementById('feed');
+        feedDiv.scrollTop = feedDiv.scrollHeight;
+      };
+    </script>
+  </head>
+<body onload="feedChat()">
 
   <nav>
     <a id="navTitle" href="/">CodeU Chat App</a>
@@ -18,6 +47,19 @@
     <a href="/about.jsp">About</a>
     <a href="/activity.jsp">Activity Feed</a>
   </nav>
+
+<div id="feed">
+  <ul>
+    <%
+      for (Conversation convo : conversations) {
+        String title = convo.getTitle();
+    %>
+    <li><strong><%= title %></strong></li>
+    <%
+      }
+    %>
+  </ul>
+</div>
 
   <div id="container">
 
