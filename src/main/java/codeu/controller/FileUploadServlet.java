4767c29aab11cce9d3e26107eda4100d358ec925
Kevin Villanueva <kevin_villanueva@umail.ucsb.edu>
2018-07-18 03:20:20 -0600

    Added the doGet in the upload servlet which serves the jsp file the image and made edits to the doPost so the media is stored.

--- src/main/java/codeu/controller/FileUploadServlet.java
+++ src/main/java/codeu/controller/FileUploadServlet.java
@@ -1,41 +1,116 @@
-import java.io.File;
+
+package codeu.controller;
+
+import codeu.model.data.Media;
+import codeu.model.data.User;
+import codeu.model.store.basic.MediaStore;
+import codeu.model.store.basic.UserStore;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
-
+import java.time.Instant;
+import java.util.UUID;
+import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
+
+/* This servlet will load upon startup and map it to the URL /FileUploadServlet. */
+@WebServlet("profile/FileUploadServlet")
+/* Max file upload size is 16 MB. */
+@MultipartConfig(maxFileSize = 16177215)
+/* Servlet Class responsible for file uploads. */ 
 public class FileUploadServlet extends HttpServlet {
 
-	@Override
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+	/* Store class that gives access to Media. */
+    private MediaStore mediaStore;
+    /* Store class that gives access to Users. */
+    private UserStore userStore;
 
-		//get the file chosen by the user
-		Part filePart = request.getPart("fileToUpload");
+
+    /**
+     * Set up state for handling activity-related requests. This method is only called when
+     * running in a server, not when running in a test.
+     */
+    @Override
+    public void init() throws ServletException {
+      super.init();
+      setMediaStore(MediaStore.getInstance());
+      setUserStore(UserStore.getInstance());
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+
+      String mediaOwner = (String)request.getSession().getAttribute("user");
+      Media picture = mediaStore.getProfilePicture(mediaOwner);
+      if (picture!=null) {
+        BufferedImage bufferedImage = picture.getContent();
+        response.setContentType(picture.getContentType());
+        String mediaExtension = picture.getContentType().substring(
+          picture.getContentType().indexOf('/')+1);
+        ImageIO.write(bufferedImage, mediaExtension, response.getOutputStream());
+      }
+    }
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+
+	    /* Title given by user in profile.jsp. */
+        String mediaTitle = (String)request.getParameter("Title"); 
+        /* Uploader. */
+        String mediaOwner = (String)request.getSession().getAttribute("user");
+        /* Input stream of the upload file. */
+        InputStream inputStream = null;
+
+		/* Get the file chosen by the user. */
+		Part filePart = request.getPart("photo");
 		
-		//get the InputStream to store the file somewhere
-	    InputStream fileInputStream = filePart.getInputStream();
+		/* Get the InputStream to store the file. */
+	    if(filePart != null) {
+	      System.out.println(filePart.getName());
+	      System.out.println(filePart.getSize());
+	      System.out.println(filePart.getContentType());
+	      //Input stream of upload file. 
+	      inputStream = filePart.getInputStream();
+	    }
 	    
-	    //for example, you can copy the uploaded file to the server
-	    //note that you probably don't want to do this in real life!
-	    //upload it to a file host like S3 or GCS instead
-	    File fileToSave = new File("/WEB-INF/view/profile.jsp" + filePart.getSubmittedFileName());
-		Files.copy(fileInputStream, fileToSave.toPath(), StandardCopyOption.REPLACE_EXISTING);
-		
-		//get the URL of the uploaded file
-		String fileUrl = "http://localhost:8080/uploaded-files/" + filePart.getSubmittedFileName();
-		
-		//You can get other form data too
-		String name = request.getParameter("name");
-		
-		//create output HTML that uses the 
-		response.getOutputStream().println("<p>Thanks " + name + "! Here's a link to your uploaded file:</p>");
-		response.getOutputStream().println("<p><a href=\"" + fileUrl + "\">" + fileUrl + "</a></p>");
-		response.getOutputStream().println("<p>Upload another file <a href=\"http://localhost:8080/index.html\">here</a>.</p>");	
+	    User user = userStore.getUser(mediaOwner);
+	    if(inputStream!=null) {
+	      BufferedImage content = ImageIO.read(inputStream);
+	      String contentType = filePart.getContentType();
+	      Media media = new Media(UUID.randomUUID(), user.getId(), 
+	        mediaTitle, Instant.now(), content, contentType);
+	      if(mediaStore.getProfilePicture(user.getName())!=null) {
+	        mediaStore.getProfilePicture(user.getName()).setIsProfilePicture(false);
+	      }
+	      mediaStore.setProfilePicture(user.getName(), media);
+	      mediaStore.addMedia(media);
+	    }
+        
+        response.sendRedirect("/profile/"+user.getName()); 
 	}
+
+	/**
+	 * Sets the MediaStore used by this servlet. This function provides a common setup method for
+	 * use by the test framework or the servlet's init() function.
+	 */
+	  void setMediaStore(MediaStore mediaStore) {
+	    this.mediaStore = mediaStore;
+	  } 
+
+	/**
+	 * Sets the UserStore used by this servlet. This function provides a common setup method for
+	 * use by the test framework or the servlet's init() function.
+	 */
+	  void setUserStore(UserStore userStore) {
+	    this.userStore = userStore;
+	  } 
 }
